08518cd115fb6bb71429aee66411f8511548ed2c
ryujiung <ryujiung@localhost>
2023-09-07 20:04:20 +0900

    mysite02

--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -6,20 +6,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Maincontroller
- */
-public class Maincontroller extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("WEB-INF/views/main/index.jsp")
-		.forward(request, response);
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+				.forward(request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		doGet(request, response);
 	}
 
