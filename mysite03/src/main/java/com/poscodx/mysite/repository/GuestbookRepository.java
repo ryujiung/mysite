54181e615a694034c7a7b1dfe9ea6b5650839878
ryujiung <ryujiung@localhost>
2023-09-19 11:59:11 +0900

    AOP 적

--- mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.poscodx.mysite.vo.GuestBookVo;
 
@@ -16,9 +17,9 @@ public class GuestbookRepository {
 	private SqlSession sqlSession;
 
 	public List<GuestBookVo> findAll() {
-
+		
 		return sqlSession.selectList("guestbook.findAll");
-
+		
 	}
 
 	public Boolean insert(GuestBookVo vo) {
